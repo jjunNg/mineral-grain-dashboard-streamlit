24d6a806223e50c519462dab9f8a97b511630fd9
jjunNg <134043844+jjunNg@users.noreply.github.com>
2025-03-15 04:00:39 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,57 @@
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+# import math
+# from pathlib import Path
+# from PIL import Image
+from ultralytics import YOLO
+
 
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+    page_title='Mineral Grain Recognition'
 )
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+st.title("Mineral grain recognition")
+st.subheader('validating BSE SEM images using YOLO model to classify images')
+uploaded_image = st.file_uploader("Upload Image")
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+# Load a pretrained YOLO11n model
+# model = YOLO("yolo11n.pt")
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+# Run inference on 'bus.jpg'
+# results = model(["https://ultralytics.com/images/bus.jpg", "https://ultralytics.com/images/zidane.jpg"])  # results list
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+# # Visualize the results
+# for i, r in enumerate(results):
+#     # Plot results image
+#     im_bgr = r.plot()  # BGR-order numpy array
+#     im_rgb = Image.fromarray(im_bgr[..., ::-1])  # RGB-order PIL image
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+#     # Show results to screen (in supported environments)
+#    # r.show()
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+#     # Save results to disk
+#     #r.save(filename=f"results{i}.jpg")
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
-# -----------------------------------------------------------------------------
-# Draw the actual page
-
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
-
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
-
-# Add some spacing
-''
-''
-
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
-
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
+ 
+# returned_image = model.predict(uploaded_image)
 
 
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
+# if uploaded_image:
+#     st.image(uploaded_image)
 
-st.header(f'GDP in {to_year}', divider='gray')
+# if returned_image:
+#     st.image(returned_image)
 
-''
 
-cols = st.columns(4)
+# col1, col2 = st.columns(2)
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+# with col1:
+#     st.header("Input image")
+#     st.image("./data/image.jpg")
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+# with col2:
+#     st.header("Returned image")
+#     st.image("returned_image")
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
